64f7efb2fecd3bc3e4bc1584caa68dc2c6b75d10
hamzamu <dr.hamzamusa@gmail.com>
2019-09-22 04:15:51 +0300

    #8 SwissMedic.ch Date grapped

--- server/swissmedic.js
+++ server/swissmedic.js
@@ -3,7 +3,7 @@ import './collections.js';
 import './db.js'
 import YAML from 'yaml';
 import log from './log.js'
-
+const cheerio = require('cheerio');
 // import URLS from './urls.yaml'
 // log('progress',URLS)
 const _ = require("lodash")
@@ -22,23 +22,23 @@ let qa = 'https://www.swissmedic.ch/swissmedic/de/home/humanarzneimittel/marktue
 let frDrugs = 'https://www.swissmedic.ch/swissmedic/fr/home/humanarzneimittel/marktueberwachung/qualitaetsmaengel-und-chargenrueckrufe/chargenrueckrufe.html';
 let frDocs = 'https://www.swissmedic.ch/swissmedic/fr/home/humanarzneimittel/marktueberwachung/health-professional-communication--hpc-.html';
 /*
-*/
+ */
 Counter = 0;
 /*
   ===
 */
 Swiss = {}
 /*
-*/
+ */
 let isSwiss = Meteor.settings.isSwiss
-if(isSwiss){
-    log('start','SwissMedic.ch Scrapping init')
+if (isSwiss) {
+  log('start', 'SwissMedic.ch Scrapping init')
   runScrapper()
 }
-
 /*
-  Development
 */
+Swiss.scrapURL = (url, id) => {
+}
 /*
   Basic App Actions
 */
@@ -48,11 +48,13 @@ Swiss.patch = (data, type, lang) => {
       title: item.title
     })
     let exclude = ["KPA Breakout Session – Präsentationen", "Newsdienste – Newsletter abonnieren"]
-    if (!isExist && exclude.indexOf(item.title) == -1) {
+    // TESTING
+    if (isExist && exclude.indexOf(item.title) == -1) {
       item.type = type;
       item.lang = lang;
       item.url = root + item.url;
       var id = Items.insert(item)
+      // Scrap DRUG
       Swiss.scrapDrug(item.url, id)
     }
   })
@@ -61,17 +63,17 @@ Swiss.patch = (data, type, lang) => {
   Write file to Disk
   => ('/export/FILENAME',data)
 */
-Swiss.writeFile = async(file, data) => {
+Swiss.writeFile = async (file, data) => {
   await fs.writeFile(path + file, data, (err) => {
-    if (err) log('error',err);
-    log('progress',"File updated" + file);
+    if (err) log('error', err);
+    log('progress', "File updated" + file);
   });
 }
 /* 
   Get Exported Files
   => [file1,file2]
 */
-Swiss.getFiles = async(dir) => {
+Swiss.getFiles = async (dir) => {
   if (!dir) return;
   return fs.readdirSync(path + dir)
 }
@@ -79,7 +81,7 @@ Swiss.getFiles = async(dir) => {
   getItems
     - Fetch collection with certain values
 */
-Swiss.getItems = async(type, lang) => {
+Swiss.getItems = async (type, lang) => {
   return Items.find({
     lang: lang,
     type: type
@@ -89,11 +91,12 @@ Swiss.getItems = async(type, lang) => {
   Custom Single Link Scrapper 
     - Puppeteer is NOT required
 */
-Swiss.scrapDrug = async(url, id) => {
+Swiss.scrapDrug = async (url, id) => {
   scrapeIt(url, {
     title: ".mod h1",
-    date: '.mod-headline h5',
     desc: ".mod-text article",
+    content : '.mod-layout .mod-text p',
+    date : '.mod.mod-headline',  
     pdf: {
       selector: ".mod-download a",
       attr: "href"
@@ -113,47 +116,51 @@ Swiss.scrapDrug = async(url, id) => {
     data,
     response
   }) => {
-    log('progress',`Scrapping:` +  data.title)
-    //log('progress',`Scrapping status: ${response.statusCode}`)
+
     data.pdf = root + data.pdf
+    var item = Items.findOne({_id:id});
+    var date = item.date[0]
+    var date = date.replace(/\n/,' ');
+    data.date = date;
+    log('progress', `Scrapped:` +  data.date + ' ' + data.title)
     Items.update({
       _id: id
     }, {
       $set: data
     })
+    //
+    log('success', `DB-Updated:` +  data.date + ' ' + data.title)
   })
 }
 /*
   Runner
 */
-async function SwissRun(){
+async function SwissRun() {
   await scrapper(swissList, 'drug', 'de')
   await scrapper(qa, 'doc', 'de')
   await scrapper(frDrugs, 'drug', 'fr')
   await scrapper(frDocs, 'doc', 'fr')
 }
-Swiss.record = async() => {
-  log('progress','Progress: Getting files ready')
+Swiss.record = async () => {
+  log('progress', 'Progress: Getting files ready')
   //Meteor.setTimeout(function () {
   await Swiss.writeFile('/exports/chargenrueckrufe_de.json', JSON.stringify(Swiss.getItems('drug', 'de')))
   await Swiss.writeFile('/exports/dhcp_hcp_de.json', JSON.stringify(Swiss.getItems('doc', 'de')))
   // French
   await Swiss.writeFile('/exports/chargenrueckrufe_fr.json', JSON.stringify(Swiss.getItems('drug', 'fr')))
   await Swiss.writeFile('/exports/dhcp_hcp_fr.json', JSON.stringify(Swiss.getItems('doc', 'fr')))
-    //Swiss.close()
-  //}, 5000)
 }
 /*
 meteor | sed -e '/Exited with code/q'
 */
-Swiss.close = async ()=>{
+Swiss.close = async () => {
   process.exit(0)
   process.kill(process.pid)
 }
 /*
   Pupeeter Scrapper
 */
-async function scrapper(url, type, lang){
+async function scrapper(url, type, lang) {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu',
@@ -164,83 +171,78 @@ async function scrapper(url, type, lang){
     ],
   });
   const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 720 });
   await page.goto(url, {
     waitUntil: 'load'
   });
   await page.waitForSelector(".mod-teaser")
+  //
   for (var i = 1; i < 7; i++) {
     if (i !== 1) {
       var resultsBox = 'a[data-loadpage = "' + i + '"]'
-      if (await page.$(resultsBox) === null){
-        log('warning','Scanning....');
-        return   
+      if (await page.$(resultsBox) === null) {
+        log('warning', 'Scanning....');
+        return
       }
       await page.click('a[data-loadpage = "' + i + '"]');
-      await page.waitFor(3000);
+      await page.waitFor(5000,{waitUntil: 'domcontentloaded', timeout: 0});
     }
+    //
     const dimensions = await page.evaluate(() => {
       var nav = document.querySelectorAll('a[data-loadpage]')
-      var ax = document.querySelectorAll('.mod-teaser')
       var title = document.querySelectorAll('.mod-teaser a')
+      var dates = document.querySelectorAll('.mod-teaser')
+      //
       var items = []
       var nav = [].map.call(nav, a => a.getAttribute("data-loadpage"));
       var nav = nav.filter(function (e) {
         return e !== 0
       })
       var nav = nav.filter((x, i, a) => a.indexOf(x) == i)
-      for (var i = 0; i < ax.length; i++) {
-        items.push({
+      for (var i = 0; i < title.length; i++) {
+        var item = {
           title: title[i].innerHTML,
           url: title[i].getAttribute('href')
-        })
+        }
+        var date =  dates[i].innerHTML
+        var date = date.replace(/<[^>]*>?/gm, '')
+        var date = date.replace(/^\s+|\s+$/g, '')
+        var date = date.split(" ")
+        item.date = date;
+        items.push(item)
       }
       return {
         items: items,
         nav: nav,
       };
     });
-    //_.each(docs)
     if (dimensions.items && dimensions.items.length) {
       Swiss.patch(dimensions.items, type, lang)
     }
     if (dimensions && dimensions.items) {
-      log('progress','Scrapped Items:' + dimensions.items.length);
+      log('progress', 'Scrapped Items:' + dimensions.items.length);
     }
   }
   await browser.close();
   Counter = Counter + 1;
-  log('success','Project ' + Counter + ' is finished')
-  //log('progress','Type:' + type + '& Lang:', lang)
-  //if(Counter == 4){
-  //}
+  log('success', 'Project ' + Counter + ' is finished')
 }
 /*
-*/
-async function runScrapper(){
+ */
+async function runScrapper() {
   await SwissRun()
   await Swiss.record()
-  await log('success','Scrapping is finished, You may close meteor')
+  await log('success', 'Scrapping is finished, You may close meteor')
   await Swiss.close()
 }
 /*
   CronJobs
 */
-// SyncedCron.add({
-//   name: 'Crunch some important numbers for the marketing department',
-//   schedule: function (parser) {
-//     return parser.text('every 12 hours');
-//   },
-//   job: function () {
-//     log('progress','Running......')
-//     Swiss.run()
-//   }
-// });
-// SyncedCron.start();
 /*
   Test
 */
 if (process.pid) {
-  log('progress','This process is your pid ' + process.pid);
+  log('progress', 'This process is your pid ' + process.pid);
 }
 /*
   Methods
@@ -248,8 +250,5 @@ if (process.pid) {
 Meteor.methods({
   getFiles() {
     return Swiss.getFiles('exports')
-  },
-  getLatest() {},
-  getStats() {},
-  fileDownload() {}
+  }
 })
