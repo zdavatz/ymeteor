f944ffb9929551fb0b07f57fda2ad41666fcb97b
hamzamu <dr.hamzamusa@gmail.com>
2020-01-23 16:22:06 +0300

    38

--- client/main.js
+++ client/main.js
@@ -8,7 +8,9 @@ import './main.html';
 import {
     Meteor
 } from 'meteor/meteor';
-import { stringify } from 'yaml';
+import {
+    stringify
+} from 'yaml';
 // import 'tabulator-tables';
 // 
 Items = new Mongo.Collection('items')
@@ -112,79 +114,76 @@ Template.registerHelper('countRow', (i) => {
 })
 /** */
 Template.registerHelper('readbaleBreaks', (str) => {
-    if(!str){
+    if (!str) {
         return
     }
-   var l = str.replace(/\Sicherheitsmerkmal Pflicht/g,"<br><strong>Sicherheitsmerkmal Pflicht</strong><br>")
+    var l = str.replace(/\Sicherheitsmerkmal Pflicht/g, "<br><strong>Sicherheitsmerkmal Pflicht</strong><br>")
     return l
 })
 /**
  * Data table header/ data
  */
- Template.registerHelper('getDataTable',(data,type,index)=>{
+Template.registerHelper('getDataTable', (data, type, index) => {
     //  if(!data || !type || !index){
     //      console.log('Error',"getDataTable Error")
     //      return
     //  }
-     var header = []
-     var rows = []
-     _.each(data,(i)=>{
-         /**
-          * Header
-          */
-         if(i[0] !== "ASK-Nr."){
+    var header = []
+    var stoffname = []
+    var stoffmenge = []
+    var rows = []
+    _.each(data, (i) => {
+        /**
+         * Header
+         */
+        if (i[0] !== "ASK-Nr." || i[1] == undefined) {
+            // set header
             if (header.indexOf(i[0]) == -1) {
                 header.push(i[0]);
             }
-            if (header.indexOf(i[1]) == -1) {
-                rows.push({[i[0]]: i[1]});
+            if (i[0] == 'Stoffname') {
+                stoffname.push(i[1])
             }
-         }
-         // Rows
-     })
-    //  console.log(header)
-    //  console.log(rows)
-
-     if(type == 'header'){
-         return header
-     }else{
-         
-        var index = index - 1;
-        console.log(rows[index])
-
-     }
-
-     
-
-
- })
-
- /**
-  * isEqual
-  */
-
-  Template.registerHelper('isEqual', (v,k)=>{
-      if(!v || !k){
-          console.log('error: isEqual')
-          return
-      }
-      if(v == k){
-          return true
-      }
-  })
-  
-
-
- /**
-  * isEqual
-  */
-
- Template.registerHelper('isNotEqual', (v,k)=>{
-    if(!v || !k){
+            if (i[0] == 'Stoffmenge') {
+                stoffmenge.push(i[1])
+            }
+        }
+    })
+    // Create rows
+    _.each(stoffname,(r,index)=>{
+        // console.log(index)
+        rows.push({name:r, dose:stoffmenge[index]})
+    })
+    console.log(header, stoffmenge, stoffname)
+    if (type == 'header') {
+        return header[index]
+    }
+    if(type == 'rows'){
+        console.log(rows)
+        return rows
+    }
+})
+/**
+ * isEqual
+ */
+Template.registerHelper('isEqual', (v, k) => {
+    if (!v || !k) {
         console.log('error: isEqual')
         return
     }
-    if(v !== k){
+    if (v == k) {
+        return true
+    }
+})
+/**
+ * isEqual
+ */
+Template.registerHelper('isNotEqual', (v, k) => {
+    if (!v || !k) {
+        console.log('error: isEqual')
+        return
+    }
+    if (v !== k) {
         return true
     }
 })
