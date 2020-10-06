11358d0e779deaa07f683630d908458efe8b3665
neox <dr.hamzamusa@gmail.com>
2020-10-06 09:12:15 +0300

    #53 Sort outcomes of SwissMedic 2

--- server/swissmedic-o.js
+++ server/swissmedic-o.js
@@ -5,14 +5,12 @@ import './config.js'
 import YAML from 'yaml'
 import './collections.js'
 import moment from 'moment';
-
 // import URLS from './urls.yaml'
 // console.log(URLS)
 const _ = require("lodash")
 const fs = require('fs')
 const scrapeIt = require("scrape-it")
 const puppeteer = require('puppeteer');
-
 log = console.log
 /*
   Project Specifics
@@ -79,24 +77,19 @@ Swiss.getItems = (type, lang) => {
         type: type
     },{sort:{dateOrder: -1}}).fetch()
 }
-
-
 console.log("DEBUG",Items.find({
     lang: 'de',
     type: 'doc'
 }).count())
-
-
 /*
   Custom Single Link Scrapper 
     - Puppeteer is NOT required
 */
-
 //  #content > div > div.col-md-8.main-content.js-glossary-context > div.mod.mod-teaserlist > div.mod.mod-dynamic.done > div:nth-child(11) > div > div > p
 Swiss.scrapDrug = (url, id) => {
     scrapeIt(url, {
         title: ".mod h1",
-        date: '.mod-headline h5',
+        dateDoc: '.mod-headline h5',
         date2: '.mod-teaser .teaserDate',
         desc: ".mod-text article",
         pdf: {
@@ -118,30 +111,22 @@ Swiss.scrapDrug = (url, id) => {
         data,
         response
     }) => {
-        
-        
         if(data.title == 'KPA Breakout Session – Präsentationen' || data.title == 'KPA Breakout Session – Présentations'){
             console.log('Data Review...')
             Items.remove(id)
         }else{
             console.log('Files updated for ',data.title)
             data.pdf = rootURL + data.pdf
-           
-
-
             // console.log({data})
             console.log('date: ',data.date, data.date2)
-
             Items.update({
                 _id: id
             }, {
                 $set: data
             })
-
             // console.log("ITEM CHECK:",Items.findOne({ _id: id}))
         }
         //console.log(`Scrapping status: ${response.statusCode}`)
-       
     })
 }
 /*
@@ -168,11 +153,8 @@ Swiss.close = async function () {
     process.exit(0)
     process.kill(process.pid)
 }
-
-
 Swiss.run = async () => {
     for (var i = 0; i <= Project.urls.length; i++) {
-
         var projects = Project.urls
         var project = projects[i]      
         if (!project || i == Project.urls.length) {
@@ -183,10 +165,7 @@ Swiss.run = async () => {
         console.log('Project:START', project.file, '---')
         await scrapper(project.url, project.type, project.lang, project.file)
     }
-
 }
-
-
 /*
   Pupeeter Scrapper
 */
@@ -208,8 +187,6 @@ let scrapper = async (url, type, lang, file) => {
     });
     // Await Data elements to be loaded
     await page.waitForSelector(".mod-teaser")
-
-
     const nav = await page.evaluate(() => {
         var nav = document.querySelectorAll('a[data-loadpage]')
         var nav = [].map.call(nav, a => a.getAttribute("data-loadpage"));
@@ -219,99 +196,69 @@ let scrapper = async (url, type, lang, file) => {
         var nav = nav.filter((x, i, a) => a.indexOf(x) == i)
         return nav;
     })
-
-
     // nav.unshift('1')
-
     console.log('Pages =>', nav)
     if (!nav.length) {
         log('error', 'Checking; There is no navigation')
         return
     }
-
-
-
-
     //
     for (var i = 0; i <= nav.length; i++) {
         console.log("PAGE: ",i)
-
         if (i == nav.length) {
             console.log('Project:FINISHED', file, 'DONE')
             console.log('----------------------------------')
             Swiss.writeFile('exports/' + file, JSON.stringify(Swiss.getItems(type, lang)))
             console.log('Check', file , " : " ,Items.find({type:type, lang:lang}).count() )
             console.log('----------------------------------')
-            
             break;
         }
-
-
         const dimensions = await page.evaluate(() => {
             var nav = document.querySelectorAll('a[data-loadpage]')
             var content = document.querySelectorAll('.mod-teaser')
             var title = document.querySelectorAll('.mod-teaser a')
             var items = []
-            
             var nav = [].map.call(nav, a => a.getAttribute("data-loadpage"));
             var nav = nav.filter(function (e) {
                 return e !== 0
             })
             var nav = nav.filter((x, i, a) => a.indexOf(x) == i)
-
-
             for (var i = 0; i < content.length; i++) {
                 var item = {
                     title: title[i].innerHTML,
                     url: title[i].getAttribute('href')
                 }
-
                 var date = content[i].innerHTML
-
                 var date = date.replace(/<[^>]*>?/gm, '')
                 var date = date.replace(/^\s+|\s+$/g, '')
                 var date = date.replace(/\n/g, '')
                 var date = date.split(" ")
                 var date = date[0]
-
                 var dateArr = date.split(".")
-                
                 var date =  dateArr.join('/')
-                
                 item.dateOrder = dateArr.reverse().join('/')
-
-                item.mainDate = date;
-
-
-
+                item.date = date;
                 console.log({item})
                 var skip = ['Newsdienste', 'Services']
                 if (skip.indexOf(date) == -1) {
                     items.push(item)
                 }
             }
-
             return {
                 items: items,
                 nav: nav,
             };
         });
-
-
-
         if (dimensions.items && dimensions.items.length) {
             Swiss.patch(dimensions.items, type, lang)
             console.log('Data Patch set:' , dimensions.items.length)
         }
-
         // if (i !== 1) {
         await page.click('a[data-loadpage = "' + nav[i] + '"]');
         console.log(' PAGE >', nav[i] , 'of' , nav)
         await page.waitFor(3000);
         // }
-
     }
-
     await browser.close();
     console.log("Project",file, ' is done');
 }
@@ -328,8 +275,6 @@ Meteor.methods({
 })
 /*
  */
-
-
 let isSwiss = Meteor.settings.isSwiss
 if (isSwiss) {
     log('start', 'SwissMedic.ch Scrapping init')
